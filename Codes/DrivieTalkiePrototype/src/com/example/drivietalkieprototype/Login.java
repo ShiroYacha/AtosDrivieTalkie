188e7524611d9fe172f4549c979e6ec77489461f
Zhao Min <sebzhaomin@gmail.com>
2013-04-14 16:13:44 +0200

    update

--- Codes/DrivieTalkiePrototype/src/com/example/drivietalkieprototype/Login.java
+++ Codes/DrivieTalkiePrototype/src/com/example/drivietalkieprototype/Login.java
@@ -1,11 +1,16 @@
 package com.example.drivietalkieprototype;
 
-import javax.security.auth.PrivateCredentialPermission;
+import java.util.LinkedHashMap;
 
+import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 
-import android.R.bool;
+import core.*;
+
+import android.R.string;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,40 +18,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import core.*;
 
 public class Login extends Activity {
+
+	protected static final Context Login = null;
+	EditText et_id;
+	EditText et_password;
+	XmppClientFacade facade;
 
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		EditText et_id;
-		EditText et_password;
+
 		Spinner  sp_server;
 		Button	btn_login;
 		super.onCreate(savedInstanceState);
+		// Populate layout
         setContentView(R.layout.login);
+        // Config widgets
         et_id=(EditText)findViewById(R.id.editText1);
         et_password=(EditText)findViewById(R.id.editText2);
         btn_login=(Button)findViewById(R.id.button1);
-        
-        final XmppClientFacade facade=new XmppClientFacade();
-        
-        final String id=et_id.getText().toString();
-        final String password=et_password.getText().toString();
+		// Set login listener
         btn_login.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				try {
-					facade.login(id, password, "Gtalk");
-				} catch (XMPPException e) {
-					// TODO: handle exception
-				}
 				
+		        String id=et_id.getText().toString();
+		        String password=et_password.getText().toString();
+	        
+		        id="shiroyacha1990@gmail.com";
+		        password = "ladivine";
+
+		    	Intent resultInt = new Intent();
+		    	resultInt.putExtra("id",id);
+		    	resultInt.putExtra("password",password);
+		    	setResult(RESULT_OK,resultInt);     
+		    	finish();
+
 			}
 		});
-
+        
+        
 	}
+	
 }
+
 
